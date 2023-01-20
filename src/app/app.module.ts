f04ae81d97521d0227d56bc344e0e21186278816
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-20 10:34:26 +0200

    Changing the language for 'Hello word" using NgrX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, StoreModule.forRoot({ message: simpleReducer })],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
